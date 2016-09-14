4c6c3586b9a430d2fab1860153d8b7b8d63b802f
Andrew Fisher and Nhat Hoang <andrew@>
2016-09-14 08:43:57 -0700

    add a cube object to the program and update the user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,3 @@
-import java.io.Console;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -13,19 +12,26 @@ public class App {
 
     get("/", (request, response)-> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/form.vtl");
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/results", (request, response) -> {
+    get("/rectangle", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       int intLength = Integer.parseInt(request.queryParams("length"));
       int intWidth = Integer.parseInt(request.queryParams("width"));
       Rectangle newRectangle = new Rectangle(intLength, intWidth);
-      boolean squareResult = newRectangle.isSquare();
-      String output = String.format("Is your rectangle a sqare too? %b", squareResult);
-      model.put("output", output);
-      model.put("template", "templates/results.vtl");
+      model.put("newRectangle", newRectangle);
+      if(newRectangle.isSquare()){
+          Cube myCube = new Cube(newRectangle);
+          model.put("myCube",myCube);
+      }
+
+
+
+      
+
+      model.put("template", "templates/rectangle.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Rectangle {
   private int mLength;
   private int mWidth;
+  private static List<Rectangle> instances = new ArrayList<Rectangle>();
+
 
   public Rectangle(int length, int width){
     mLength = length;
     mWidth = width;
+    instances.add(this);
   }
   public int getLength(){
     return mLength;
@@ -14,5 +20,12 @@ public class Rectangle {
   }
   public boolean isSquare(){
     return mLength == mWidth;
+  }
+  public int area() {
+    return mLength * mWidth;
+  }
+
+  public static List<Rectangle> all() {
+  return instances;
   }
 }
